12762c184a83960e397e681d00ba5c68f385fdbe
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2026-07-29 11:05:02 -0300

    Add power function to calculator

--- test_calculator.py
+++ test_calculator.py
@@ -22,3 +22,7 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         calculator.divide(1, 0)
+
+
+def test_power():
+    assert calculator.power(2, 3) == 8
