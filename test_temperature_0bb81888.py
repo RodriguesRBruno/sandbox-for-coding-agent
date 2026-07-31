d69e599278871e6696e1f66a5f40d4c2be59c45b
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2026-07-31 11:39:55 -0300

    Add test_temperature_SFX.py (clean fixture)

---
+++ test_temperature_0bb81888.py
@@ -0,0 +1,9 @@
+from temperature_0bb81888 import celsius_to_fahrenheit, fahrenheit_to_celsius
+
+
+def test_celsius_to_fahrenheit():
+    assert celsius_to_fahrenheit(100) == 212
+
+
+def test_fahrenheit_to_celsius():
+    assert fahrenheit_to_celsius(32) == 0
