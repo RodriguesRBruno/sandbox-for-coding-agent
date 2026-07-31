3624ad7259f62a3df7b32772dbd2490b7432a3da
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2026-07-31 09:36:53 -0300

    Add temperature.py (clean fixture)

---
+++ temperature.py
@@ -0,0 +1,9 @@
+"""Temperature conversion helpers."""
+
+
+def celsius_to_fahrenheit(celsius: float) -> float:
+    return celsius * 9 / 5 + 32
+
+
+def fahrenheit_to_celsius(fahrenheit: float) -> float:
+    return (fahrenheit - 32) * 5 / 9
