f348e61bb1e4f8b1651fe82f592f8d2af9a8cecb
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2026-08-03 15:20:00 -0300

    Mock files: this PR should fail on all accounts

---
+++ secure_file.py
@@ -0,0 +1,7 @@
+AWS_SECRET_KEY = 'super_duper_secret'
+
+def main():
+ print(f'The secret key {AWS_SECRET_KEY} is super safe!')
+
+if __name__ == '__main__':
+  main()
---
+++ test_secure_file.py
@@ -0,0 +1,2 @@
+def test_safe_test():
+    assert 1 == 2
