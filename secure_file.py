4e5d8b3916fae3aee9cb20620ea85d626f71af84
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2026-08-03 15:49:36 -0300

    lint away

--- secure_file.py
+++ secure_file.py
@@ -1,5 +1,6 @@
 def main():
- print(f'The secret key is even safer; not mentioned here at all!!')
+    print(f"The secret key is even safer; not mentioned here at all!!")
 
-if __name__ == '__main__':
-  main()
+
+if __name__ == "__main__":
+    main()
